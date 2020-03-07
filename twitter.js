9e94c310399cdec660b46135e7b5bee3d20d6f9f
developinggus <55714431+developinggus@users.noreply.github.com>
2020-03-06 23:21:13 -0500

    Create twitter.js

---
+++ twitter.js
@@ -0,0 +1,37 @@
+var Twit = require('twit');
+
+var config = require('./config')
+
+var T = new Twit(config);
+
+setInterval(tweetIt, 1000 * 60)
+
+function tweetIt() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var mo= date.getMonth();
+  var da = date.getDate();
+  if ( h == SOME HOUR && m == SOME MINUTE) {
+    var r = Math.floor(Math.random()*100);
+    if ( mo == SOME MONTH && da == SOME DATE ) {
+      var tweet = {
+        status: 'SOME TWEET'
+      }
+    } else {
+      var tweet = {
+        status: 'SOME TWEET'
+      }
+    }
+    T.post('statuses/update', tweet, tweeted);
+
+    function tweeted(err,data,response) {
+      if (err) {
+        console.log("Oops");
+      } else {
+        console.log("Works");
+      }
+    //console.log(data)
+    }
+  }
+}
